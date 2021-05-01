4cb9e97094ec560fd1f428598a582da63703633d
sanggoe <sanggoe0509@gmail.com>
2021-05-01 23:56:02 +0900

    - create SecurityConfig.java - edit home view

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,19 +1,13 @@
 package io.security.corespringsecurity.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping(value="/")
 	public String home() throws Exception {
-		return "home";
-	}
-
-	@PostMapping(value="/")
-	public String postHome() throws Exception {
 		return "home";
 	}
 
