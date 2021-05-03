818e4b8db9da1ed4d0addc19a7d60145bf952a05
sanggoe <sanggoe0509@gmail.com>
2021-05-03 20:54:04 +0900

    edit SecurityConfig file - 계정 생성 및 권한 부여 - 페이지별 권한 부여 설정

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,20 +1,48 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // id, password 계정 생성 및 권한 부여
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // password 암호화
+        String password = passwordEncoder().encode("1111");
+
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    }
+
+    // password 암호화 해주는 객체 반환
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                // 페이지별 접근 권한 설정
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and()
+
+        .and()
                 .formLogin();
     }
 }
