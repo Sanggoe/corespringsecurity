3a0aeb86971fa31428d47929ca5231851a8734a4
sanggoe <sanggoe0509@gmail.com>
2021-06-05 19:14:24 +0900

    DB 연동 웹 계층 구현 - edit domain Account, AccountDto - create domain entity Resources, Role - create domain dto ResourcesDto, RoleDto

--- src/main/java/io/security/corespringsecurity/domain/dto/RoleDto.java
+++ src/main/java/io/security/corespringsecurity/domain/dto/RoleDto.java
@@ -1,22 +1,18 @@
 package io.security.corespringsecurity.domain.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AccountDto {
+public class RoleDto {
 
     private String id;
-    private String username;
-    private String email;
-    private int age;
-    private String password;
-    private List<String> roles;
+    private String roleName;
+    private String roleDesc;
 }
